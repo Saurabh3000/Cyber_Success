ecca88354dc4e9549d5f3e76c8a2dccc7076fc39
Saurabh Chavan <68920063+Saurabh3000@users.noreply.github.com>
2026-01-23 11:33:30 +0530

    character_datatype_program

---
+++ Variable/Char1.java
@@ -0,0 +1,13 @@
+class Char1
+{
+	public static void main(String[] args)
+	{
+		char c1 = 'A';
+		char c2 = '2';
+		char c3 = '#';
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+	}
+}
