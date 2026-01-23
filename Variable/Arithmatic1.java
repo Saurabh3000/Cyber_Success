d237c76501ee59ac4522fece1ed55bf8a851875e
Saurabh Chavan <68920063+Saurabh3000@users.noreply.github.com>
2026-01-23 11:34:58 +0530

    arithmatic_operator_example

---
+++ Variable/Arithmatic1.java
@@ -0,0 +1,21 @@
+class Arithmatic1
+{
+	public static void main(String[] args)
+	{
+		int result;
+		result = 10 + 20;  
+		System.out.println("Addition = "+result);
+		
+		result = 20 - 10;
+		System.out.println("Substraction = "+result);
+		
+		result = 10 * 2;
+		System.out.println("Multiplication = "+result);
+		
+		result = 10 / 2;
+		System.out.println("Division = "+result);
+		
+		result = 10 % 2 ;
+		System.out.println("Modulas = "+result);
+	}
+}
