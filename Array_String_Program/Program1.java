074d44823db27d78e567e6d7cb58b4818640fe67
Saurabh Chavan <68920063+Saurabh3000@users.noreply.github.com>
2026-04-04 10:19:46 +0530

    all_array_elements

---
+++ Array_String_Program/Program1.java
@@ -0,0 +1,18 @@
+class Program1
+{
+    public static void main(String[] args) {
+         int[] arr = {10, 20,30,40,50};
+
+         for(int i=0; i<arr.length; i++)
+         {
+            System.out.println(arr[i]);
+         }
+
+         System.out.println("----------");
+
+         for(int i : arr)
+         {
+            System.out.println(i);
+         }
+    }
+}
